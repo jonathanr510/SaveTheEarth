59a850394651eecabb955fdd8ff1d1133fe25925
Student <student@Admins-MacBook-Air-9.local>
2019-06-08 15:51:02 -0700

    Initial commit

---
+++ app/src/main/java/com/jonny/android/savetheearth/climatechange.java
@@ -0,0 +1,16 @@
+package com.jonny.android.savetheearth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class climatechange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_climatechange);
+    }
+
+}
---
+++ app/src/main/java/com/jonny/android/savetheearth/trash.java
@@ -0,0 +1,13 @@
+package com.jonny.android.savetheearth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class trash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trash);
+    }
+}
